7098e692b09f4e300767ceb9934d8559642193fb
thelucazip <thelucazip@gmail.com>
2023-03-22 23:48:47 -0300

    deploy contracts

--- test/Contract.test.ts
+++ test/Contract.test.ts
@@ -4,8 +4,10 @@ import { expect } from "chai";
 
 import { ethers } from "hardhat"
 
+const oneWeek = 604800;
+const amountToTest = ethers.utils.parseEther("1.0");
+
 describe("Accountability contract", function () {
-  // Should be able to deploy the contract
   async function deployContractAccountability() {
     const AccountabilityFactory = await ethers.getContractFactory("Accountability");
     const AccountabilityNFTsFactory = await ethers.getContractFactory("AccountabilityNFTs");
@@ -32,18 +34,58 @@ describe("Accountability contract", function () {
     return { accountability, accountabilityNFTs, owner, addr1, addr2, addr3 };
   }
 
-  // should be able to deposit funds using the deposit function
   it("Should be able to deposit funds", async function () {
-    const { accountability, accountabilityNFTs, owner, addr1, addr2, addr3 } = await loadFixture(deployContractAccountability);
+    const { accountability, owner } = await loadFixture(deployContractAccountability);
 
-    const oneWeek = 604800;
+    await accountability.deposit(oneWeek, { value: amountToTest });
 
-    await accountability.deposit(oneWeek, { value: ethers.utils.parseEther("1.0") });
+    const depositedFunds = (await accountability.lockedFunds(owner.address)).amount;
+    const lockedTime = (await accountability.lockedFunds(owner.address)).lockedFor;
+
+    expect(depositedFunds).to.equal(amountToTest);
+    expect(lockedTime).to.equal(oneWeek);
 
   });
-  // should reject a withdrawal if the user doesn't have an NFT from the contract
 
-  // should reject a withdrawal if the user has 0 funds
+  it("Should reject a withdrawal if the user doesn't have an NFT from the contract or the time hasnt passed yet", async function () {
+    const { accountability } = await loadFixture(deployContractAccountability);
 
+    await accountability.deposit(oneWeek, { value: amountToTest });
+
+    // reject withdrawal if the time hasnt passed yet
+    await expect(accountability.withdraw()).to.be.revertedWith("You need to wait until your ETH is unlocked");
+
+    // simulate passing the time for one week
+    await ethers.provider.send("evm_increaseTime", [oneWeek]);
+
+    // reject withdrawal if the user doesn't have an NFT from the contract
+    await expect(accountability.withdraw()).to.be.revertedWith("You need to own an NFT to withdraw your ETH");
+  });
+
+  it("Should reject a withdrawal if the user has 0 funds", async function () {
+    const { accountability } = await loadFixture(deployContractAccountability);
+
+    await expect(accountability.withdraw()).to.be.revertedWith("You don't have any ETH locked up");
+  });
+
+  // Succesfully withdraw funds if the user has an NFT and the time has passed
+  it("Should be able to withdraw funds", async function () {
+    const { owner, accountabilityNFTs, accountability } = await loadFixture(deployContractAccountability);
+
+    await accountability.deposit(oneWeek, { value: amountToTest });
+
+    await accountabilityNFTs.mintTo(owner.address, "URI");
+
+    // simulate passing the time for one week
+    await ethers.provider.send("evm_increaseTime", [oneWeek]);
+
+    // Now we should be able to withdraw the funds
+    await accountability.withdraw();
+
+    // Expect the lockedFunds mapping to have 0 funds
+    const depositedFunds = (await accountability.lockedFunds(owner.address)).amount;
+    expect(depositedFunds).to.equal(0);
+
+  });
   // maybe lets try replicate some kind of reentrancy attack
 });
